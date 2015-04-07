a9bb24734f6154f5ff13aabf04a54373a6179bf7
sudamitsuhiro <suda@appirits.com>
2015-04-07 12:28:37 +0900

    Js修正

--- app/assets/javascripts/products.js
+++
@@ -1,8 +0,0 @@
-
-    $(function(){
-        $("#acMenu dt").on("click", function() {
-            $(this).next().slideToggle();
-            $(this).toggleClass("active");//追加部分
-        });
-    });
-
